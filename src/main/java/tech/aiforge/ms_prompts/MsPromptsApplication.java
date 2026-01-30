d1a01405bbae5c00025d7ed3615365c370f62b4e
mahmooddev01 <devmomo111@gmail.com>
2026-01-30 23:41:35 +0000

    Initial commit

---
+++ src/main/java/tech/aiforge/ms_prompts/MsPromptsApplication.java
@@ -0,0 +1,13 @@
+package tech.aiforge.ms_prompts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPromptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPromptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/aiforge/ms_prompts/MsPromptsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.aiforge.ms_prompts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPromptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
